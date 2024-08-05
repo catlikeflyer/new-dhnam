eb9398a1d52102a6cfa89f6cf3ce88379c79c4a7
Do Hyun Nam <dollis2002@gmail.com>
2024-08-04 21:40:39 -0600

    Changed padding

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
   return (
-    <nav className="bg-gray-gradient text-black fixed top-0 left-0 bg-gray-200 w-full h-16 z-50">
+    <nav className="bg-gray-gradient text-black fixed top-0 left-0 bg-gray-200 w-full h-16 z-50 p-4">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         <div className="flex items-center">
           <h1 className="text-2xl font-black uppercase text-indigo-600">
